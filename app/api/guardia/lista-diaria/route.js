e8e2d0bfbd8151e4916df50422f3da2c90c701b0
piscinasurchile-lang <piscinasurchile@gmail.com>
2026-09-25 23:48:42 -0300

    Use effective replacement OBAC in daily guard list

--- app/api/guardia/lista-diaria/route.js
+++ app/api/guardia/lista-diaria/route.js
@@ -47,7 +47,7 @@ export async function GET(request) {
         gr.actualizado_en AS reemplazo_actualizado_en
       FROM guardia_inscripciones gi
       LEFT JOIN guardia_confirmaciones gc
-        ON gc.fecha = gi.fecha AND gc.codigo = gi.codigo
+        ON gc.fecha = gi.fecha AND gc.codigo = COALESCE(gr.codigo_reemplazo,gi.codigo)
       LEFT JOIN guardia_reemplazos gr
         ON gr.fecha = gi.fecha AND gr.codigo_original = gi.codigo
       WHERE gi.fecha = ${fecha}::date
